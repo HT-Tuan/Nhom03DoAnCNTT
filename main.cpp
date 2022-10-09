ac536ef43b22e4efb9e3952db99b4fe27d35d711
ThanhTuan <huynhthanhtuan2206@gmail.com>
2022-10-09 15:54:57 +0700

    update file main class queue and stack

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
-#include <stdio.h>
 #include <malloc.h>
 #include <iostream>
 using namespace std;
-//
+
 // Lop NODE
 template <typename T>
 class Node
@@ -13,18 +12,147 @@ public:
     T data;
     Node *left, *right, *parent;
     // constructor
-    Node(T data)
+    Node()
     {
-        Creat_Node(data);
+        Creat_Node();
     }
     //
-    void Creat_Node(T data)
+    void Creat_Node()
     {
-        this->data = data;
         this->left = NULL;
         this->right = NULL;
         this->parent = NULL;
     }
+};
+//
+// Node dang ds lien ket dung cho Queue va stack
+template <typename T>
+class Node_list
+{
+public:
+    // Thuoc tinh
+    Node<T> data;
+    Node_list *next;
+    //
+    Node_list(Node<T> data)
+    {
+        Creat_Node_list(data);
+    }
+    // Phuong thuc
+    void Creat_Node_list(Node<T> data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+//
+template <typename T>
+class Queue
+{
+public:
+    Queue()
+    {
+        Create_Queue();
+    }
+    // Phuong thuc
+    // Enqueue
+    int PushQ(Node<T> value)
+    {
+        Node_list<T> *p;
+        p = (Node_list<T> *)malloc(sizeof(Node_list<T>));
+        if (p == NULL)
+        {
+            cout << "\nCo loi! khong the cap phat bo nho." << endl;
+            return 0;
+        }
+        p->Creat_Node_list(value);
+        if (this->Front == NULL)
+        {
+            this->Front = p;
+            this->Rear = p;
+        }
+        else
+        {
+            (this->Rear)->next = p;
+            this->Rear = p;
+        }
+        return 1;
+    }
+    // Dequeue
+    int PopQ(Node<T> *value)
+    {
+        Node_list<T> *p;
+        p = this->Front;
+        if (p != NULL)
+        {
+            *value = p->data;
+            this->Front = p->next;
+            if (this->Front == NULL)
+            {
+                this->Rear = NULL;
+            }
+            free(p);
+            return 1;
+        }
+        return 0;
+    }
+
+private:
+    // Thuoc tinh
+    Node_list<T> *Front;
+    Node_list<T> *Rear;
+    // constructor
+    void Create_Queue()
+    {
+        this->Front = NULL;
+        this->Rear = NULL;
+    }
+};
+//
+template <typename T>
+class Stack
+{
+public:
+    Stack()
+    {
+        Create_Stack();
+    }
+    void Create_Stack()
+    {
+        this->sp = NULL;
+    }
+    // Them vao dinh stack
+    int PushS(Node<T> value)
+    {
+        Node_list<T> *p;
+        p = (Node_list<T> *)malloc(sizeof(Node_list<T>));
+        if (p == NULL)
+        {
+            cout << "\nCo loi! khong the cap phat bo nho." << endl;
+            return 0;
+        }
+        p->data = value;
+        p->next = this->sp;
+        this->sp = p;
+        return 1;
+    }
+    // lay tu dinh stack
+    int PopS(Node<T> *value)
+    {
+        Node_list<T> *p;
+        p = this->sp;
+        if (p != NULL)
+        {
+            *value = (this->sp)->data;
+            this->sp = p->next;
+            free(p);
+            return 1;
+        }
+        return 0;
+    }
+
+private:
+    Node_list<T> *sp;
 };
 //
 // Lop Cay binary search tree (BST)
@@ -69,12 +197,13 @@ private:
         Node<T> *x, *y, *z;
         // z = new Node<T>(value);
         z = (Node<T> *)malloc(sizeof(Node<T>));
-        z->Creat_Node(value);
         if (z == NULL)
         {
-            printf("\nCo loi! khong the cap phat bo nho.\n");
+            cout << "\nCo loi! khong the cap phat bo nho." << endl;
             return;
         }
+        z->Creat_Node();
+        z->data = value;
         y = NULL;
         x = Root;
         while (x != NULL)
@@ -125,19 +254,20 @@ private:
         }
     }
 };
+
 int main()
 {
-    BST<int> Tree;
-    Tree.InsertTree(12);
-    Tree.InsertTree(18);
-    Tree.InsertTree(5);
-    Tree.InsertTree(2);
-    Tree.InsertTree(9);
-    Tree.InsertTree(15);
-    Tree.InsertTree(19);
-    Tree.InsertTree(17);
-    Tree.DuyetGiua();
-    // cout<<endl;
+    // BST<int> Tree;
+    // Tree.InsertTree(12);
+    // Tree.InsertTree(18);
+    // Tree.InsertTree(5);
+    // Tree.InsertTree(2);
+    // Tree.InsertTree(9);
+    // Tree.InsertTree(15);
+    // Tree.InsertTree(19);
+    // Tree.InsertTree(17);
+    // Tree.DuyetGiua();
+    // cout << endl;
     // Tree.DuyetGiua();
     // cout<<endl;
     // //
@@ -167,5 +297,24 @@ int main()
     // Tree2.InsertTree(12.8);
     // Tree2.DuyetGiua();
     // cout<<endl;
+    //================= test Queue - Stack ===================
+    // Node<int> x, y, z, temp;
+    // x.data = 1;
+    // y.data = 2;
+    // z.data = 3;
+    // Stack<int> s1;
+    // s1.PushS(x);
+    // s1.PushS(y);
+    // s1.PushS(z);
+    // s1.PopS(&temp);
+    // cout << "stack:" << temp.data << endl;
+    // Queue<int> q1;
+    // q1.PushQ(x);
+    // q1.PushQ(y);
+    // q1.PushQ(z);
+    // q1.PopQ(&temp);
+    // cout << "Queue:" << temp.data << endl;
+    //==================
+    
     return 0;
 }
